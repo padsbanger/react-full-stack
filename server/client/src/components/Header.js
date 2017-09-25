9fb23bc071f893c5dcd394161dc162ba185d8dc5
Michał Lach <kontakt@michal-lach.pl>
2017-09-25 22:35:29 +0200

    auth reducer

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+  renderContent() {
+    const { auth } = this.props;
+    return auth
+      ? <li><a href="/api/logout">Logout</a></li>
+      : <li><a href="/auth/google">Login</a></li>
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">Logo</a>
           <ul id="nav-mobile" className="right">
-            <li><a href="badges.html">Login</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +24,10 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+  return {
+    auth
+  }
+}
+
+export default connect(mapStateToProps)(Header);
